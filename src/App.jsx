80164d82c545f7ce90118bc0ef8c2ced8db13c45
Hamdan0007 <hamdanyaseen99040@gmail.com>
2024-07-15 17:23:27 +0500

    satart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Gallery from './pages/Gallery'
+import Donate from './pages/Donate'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/gallery" element={<Gallery/>} />
+        <Route path="/donate" element={<Donate/>} />
+
+ 
       </Routes>
     </div>
+    
   )
 }
 
---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+import '../App'; // Import the CSS file
+
+const Gallery = () => {
+  return (
+    <>
+      
+    <Navbar/>
+    
+    <div>
+
+    <div className='border bg-blue-600 text-white text-center p-6 justify-center items-center flex flex-col'>
+    <div className="slim-line"></div>
+
+      <h1 className='font-extrabold text-5xl p-3'>Gallery</h1>
+      <p className='p-3 w-[70%]'>Dedicated to combating drug addiction, our agency provides comprehensive prevention, treatment, and rehabilitation services to foster healthier, drug-free communities."</p>
+     </div>
+
+    </div>
+
+
+    <Footer/>
+    
+    
+    </>
+    )
+    
+}
+
+export default Gallery
