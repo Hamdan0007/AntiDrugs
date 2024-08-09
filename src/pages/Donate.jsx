77485965e82577294a7a9c8ee6baaad376f1d1ed
Hamdan0007 <hamdanyaseen99040@gmail.com>
2024-08-09 15:50:08 +0500

    changes in Donation Content

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -35,14 +35,14 @@ const Donate = () => {
 
               <h3 className='font-bold text-4xl mb-6 text-[#353980]'>For Donations to Zindagi Welfare Organization</h3>
 <ul className=' space-y-3 text-lg' >
-<li><strong className='text-[#353980]'>Bank:</strong> Habib Bank Limited</li>
-<li><strong className='text-[#353980]'>Address:</strong>&nbsp;Sector B-1, Phase V Hayatabad Peshawar Branch</li>
-<li><strong className='text-[#353980]'>Account Number: </strong>1113-00045819-01</li>
-<li><strong className='text-[#353980]'>Title of account:</strong> Zindagi WELFARE FOUNDATION</li>
-<li><strong className='text-[#353980]'>Swift Code: </strong>HABBPKKA</li>
-<li><strong className='text-[#353980]'>IBAN Code:</strong>&nbsp;PK32HABB000930283920390</li>
-<li><strong className='text-[#353980]'>Branch Code:</strong>&nbsp;1113</li>
-<li><strong className='text-[#353980]'>Telephone: </strong>+92-093273872</li>
+<li><strong className='text-[#353980]'>Bank:</strong> ABL Allied Bank limited</li>
+<li><strong className='text-[#353980]'>Branch Address:</strong>SHOP # T-488, MAIN BAZAR KOHAT.</li>
+<li><strong className='text-[#353980]'>Account Number: </strong>  0010065299830011</li>
+<li><strong className='text-[#353980]'>Title of account:</strong> Farid Ullah</li>
+<li><strong className='text-[#353980]'>Swift Code: </strong>PK60ABPA</li>
+<li><strong className='text-[#353980]'>IBAN Number:</strong>PK60ABPA0010065299830011</li>
+<li><strong className='text-[#353980]'>Branch Code:</strong>185</li>
+<li><strong className='text-[#353980]'>ABL Branch Phone number: </strong>0922-518709</li>
 
 </ul>
               </div>
