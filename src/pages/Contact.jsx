ae457cb2a0bcb96ef5a6894f13065590bac8120b
Hamdan0007 <hamdanyaseen99040@gmail.com>
2024-08-17 15:08:46 +0500

    changes contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,52 +21,85 @@ const Contact = () => {
       <p className='p-3 md-w-[70%] text-center'>Reach out to us for support, resources, or more information about our drug addiction prevention and treatment services."</p>
      </div>
 
-
      </div>
 
 <div>
 
-<section className="text-gray-600 body-font relative">
-  <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-    <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-      <iframe width="100%" height="100%" className="absolute inset-0" style={{filter: 'yellowscale(0.5) contrast(1.2) opacity(0.4)'}}  title="map"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d106390.41357584005!2d71.46753260000001!3d33.56115905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38d8ef13ac5af415%3A0x864572b0758eb834!2sKohat%2C%20Khyber%20Pakhtunkhwa!5e0!3m2!1sen!2s!4v1722961788868!5m2!1sen!2s" />
-      <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-        <div className="lg:w-1/2 px-6">
-          <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-          <p className="mt-1">Gulberg Colony, House No#3, University Road, Near Malak CNG Kohat City.</p>
+<section className="bg-white ">
+ 
+
+  <div className="container px-6 py-12 mx-auto">
+    <div className="lg:flex lg:items-center lg:-mx-6">
+      <div className="lg:w-1/2 lg:mx-6">
+        <h1 className="text-2xl font-semibold text-gray-800 capitalize dark:text-white lg:text-3xl">
+          Contact us for more info
+        </h1>
+        <div className="mt-6 space-y-8 md:mt-8">
+          <p className="flex items-start -mx-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            <span className="mx-2 text-gray-700  w-72 ">
+            Gulberg Colony, House No#3, University Road, Near Malak CNG Kohat City.
+            </span>
+          </p>
+          <p className="flex items-start -mx-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+            </svg>
+            <span className="mx-2 text-gray-700 w-72 ">03337000686 /03100010086 / 03229155072</span>
+          </p>
+          <p className="flex items-start -mx-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
+            <span className="mx-2 text-gray-700  w-72 ">zindagiwelfareorg.pk@gmail.com
+</span>
+          </p>
         </div>
-        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-          <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-          <a className="text-indigo-500 leading-relaxed">zindagiwelfareorg.pk@gmail.com</a>
-          <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-          <p className="leading-relaxed">03337000686 /03100010086 /
-03229155072</p>
+      
+
+      </div>
+      <div className="mt-8 lg:w-1/2 lg:mx-6">
+        <div className="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl  lg:max-w-xl shadow-gray-300/50 ">
+          <h1 className="text-lg font-medium text-gray-700">ZINDAGI Welfare Organization (ZWO) empowers marginalized groups in Pakistan through comprehensive, rights-based services and community support.</h1>
+          <form className="mt-6">
+            <div className="flex-1">
+              <label className="block mb-2 text-sm text-gray-900 ">Full Name</label>
+              <input type="text" placeholder="Full Name" className="block w-full px-5 py-3 mt-2 text-gray-900 placeholder-gray-400 bg-white border border-gray-200 rounded-md     focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+            </div>
+
+            <div className="flex-1 mt-6">
+              <label className="block mb-2 text-sm text-gray-900 ">Email address</label>
+              <input type="email" placeholder="johndoe@example.com" className="block w-full px-5 py-3 mt-2 text-gray-900 placeholder-gray-400 bg-white border border-gray-200 rounded-md     focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+            </div>
+            <div className="w-full mt-6">
+              <label className="block mb-2 text-sm text-gray-900 ">Message</label>
+              <textarea className="block w-full h-32 px-5 py-3 mt-2 text-gray-900 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48     focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message" defaultValue={""} />
+            </div>
+            <button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+              get in touch
+            </button>
+          </form>
         </div>
       </div>
     </div>
-    <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-      <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p className="leading-relaxed mb-5 text-gray-600">ZINDAGI Welfare Organization (ZWO) empowers marginalized groups in Pakistan through comprehensive, rights-based services and community support.</p>
-      <div className="relative mb-4">
-        <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-        <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" defaultValue={""} />
-      </div>
-      <button className="text-white bg-[#353980] border-0 py-2 px-6 focus:outline-none hover:bg-green-800 rounded text-lg">Button</button>
-     </div>
   </div>
+
+
+
+
+<div className='h-96'>
+      
+      <iframe width="100%" height="100%" className="" style={{filter: 'yellowscale(0.5) contrast(1.2) opacity(0.4)'}}  title="map"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d106390.41357584005!2d71.46753260000001!3d33.56115905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38d8ef13ac5af415%3A0x864572b0758eb834!2sKohat%2C%20Khyber%20Pakhtunkhwa!5e0!3m2!1sen!2s!4v1722961788868!5m2!1sen!2s" />
+    
+        </div>  
+
 </section>
 
 
 </div>
-
 
     </div>
 
