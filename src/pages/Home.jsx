0a29fe0d83f8ff3a3e0f611c785aac6bd9e3e338
Zohaib <apricopt@gmail.com>
2024-08-20 21:31:35 +0500

    button link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import '../App.css'
 // import Donor from '../Components/Donor'
 import Footer from '../Components/Footer'
@@ -21,8 +22,8 @@ function Home() {
       <h1 className="title-font sm:text-6xl text-3xl mb-4 font-extrabold text-gray-100">Zindagi Welfare Organization</h1>
       <p className="mb-8 leading-relaxed">ZINDAGI Welfare Organization (ZWO) is a charitable, not-for-profit organization, for providing services to the most marginalized and vulnerable groups in society.</p>
       <div className="flex justify-center">
-        <button className="inline-flex text-white bg-[#353980] border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Us</button>
-        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">About Us</button>
+        <Link to="/donate" className="inline-flex text-white bg-[#353980] border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Us</Link>
+        <Link to="/about" className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">About Us</Link>
       </div>
     </div>
   </div>
